df66f596c277816f933e80eb83129deb5034da76
govardhan <govardhan@gmail.com>
2023-12-18 23:26:29 +0530

    first commit

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -50,7 +50,7 @@ systemctl start rabbitmq-server  &>> $LOGFILE
 
 VALIDATE $? "Starting rabbitmq server"
 
-rabbitmqctl -r add_user roboshop roboshop123 &>> $LOGFILE
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
 
 VALIDATE $? "creating user"
 
